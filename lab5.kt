e160fe1eb920eacd2eb1b89c6990e9bc3a781a37
GataullinaLili <127747080+GataullinaLili@users.noreply.github.com>
2025-04-29 10:19:53 +0300

    Create lab5.kt

---
+++ lab5.kt
@@ -0,0 +1,8 @@
+fun main() {
+    val a = intArrayOf(24, -63, 67, -12, 88, 94, -28, 82, 0, 53)
+    a.sort()
+    
+    print("Массив данных в порядке возрастания: ")
+    println(a.contentToString())
+
+}

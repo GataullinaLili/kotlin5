78b7db67056d6f5cd910d3d2d6a4cec6f022ca78
GataullinaLili <127747080+GataullinaLili@users.noreply.github.com>
2025-04-29 09:17:51 +0300

    Update lab1.kt

--- lab1.kt
+++ lab1.kt
@@ -2,6 +2,6 @@ fun main() {
     val a = 5
     val b = 8
     val num = (minOf(a, b)..maxOf(a, b)).toList()
-    println("Введеные числа: $a , $b")
+    println("Числа: $a , $b")
     println("Массив: ${num.joinToString(", ")}")
 }
